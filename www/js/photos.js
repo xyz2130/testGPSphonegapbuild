4cb48b76a74823fb0851b83cd117dc79802b092b
Mai <prakunchan@myvuw.ac.nz>
2014-06-19 02:55:43 +1200

    fix take picture bug

--- www/js/photos.js
+++ www/js/photos.js
@@ -77,8 +77,10 @@ function isIOS() {
         options = { quality: 50,
 			destinationType : Camera.DestinationType.FILE_URI,
 			sourceType : Camera.PictureSourceType.CAMERA,
+			targetWidth: 500,
+			targetHeight: 500,
 			allowEdit: true,
-			//correctOrientation: true,
+			correctOrientation: true,
 			encodingType: Camera.EncodingType.JPG,
 			saveToPhotoAlbum: true };
 		
@@ -222,9 +224,51 @@ function isIOS() {
 	}
 	
 	function rmFail(message){
-		alert('image delete failed '+message.code);
+		alert('image delete failed: '+getFileErrMsg(message.code));
 	}
 	
+	function getFileErrMsg(code){
+		var msg = '';       
+		switch (e.code)
+		{
+		case FileError.QUOTA_EXCEEDED_ERR: 
+		msg = 'QUOTA_EXCEEDED_ERR';
+		break;
+        case FileError.NOT_FOUND_ERR:
+		msg = 'NOT_FOUND_ERR';
+		break;
+        case FileError.SECURITY_ERR:
+		msg = 'SECURITY_ERR';
+		break;
+        case FileError.INVALID_MODIFICATION_ERR:
+		msg = 'INVALID_MODIFICATION_ERR';
+		break;
+        case FileError.INVALID_STATE_ERR:
+		msg = 'INVALID_STATE_ERR';
+		break;
+		case FileError.ABORT_ERR:
+		msg = 'ABORT_ERR';
+		break;
+		case FileError.NOT_READABLE_ERR:
+		msg = 'NOT_READABLE_ERR';
+		break;
+		case FileError.ENCODING_ERR:
+		msg = 'ENCODING_ERR';
+		break;
+        case FileError.SYNTAX_ERR:
+		msg = 'SYNTAX_ERR';
+		break;
+		case FileError.TYPE_MISMATCH_ERR:
+		msg = 'TYPE_MISMATCH_ERR';
+		break;
+        case FileError.PATH_EXISTS_ERR:
+		msg = 'PATH_EXISTS_ERR';
+		break;
+        default:
+		msg = e.code;
+        break; 
+		};
+	}
 	/////////////////////////////////////////////////////
 	// upload photo code template from:
 	// http://docs.phonegap.com/en/3.1.0/cordova_file_file.md.html#FileTransfer
